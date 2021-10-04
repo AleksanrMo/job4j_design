4c2c29ada6eb39f40e59b5c439e230fa48c4e9eb
AleksandrMo <sushanta@mail.ru>
2021-10-04 08:51:28 +0700

    Создать модель User

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Objects;
+
+public class User {
+
+
+    private String name;
+    private int children;
+    private Calendar calendar;
+
+    public User(String name, int children, Calendar calendar) {
+        this.name = name;
+        this.children = children;
+        this.calendar = calendar;
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "children="
+                + children
+                + ", name='"
+                + name + '\''
+                + ", calendar="
+                + calendar.getTime()
+                +
+                '}';
+    }
+}
