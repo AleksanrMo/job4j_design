0cb78d547659c4821555896845700a747b4913e4
AleksandrMo <sushanta@mail.ru>
2021-10-12 23:55:31 +0700

    FileOutputStream

--- src/main/java/ru/job4j/io/MultiplyTable.java
+++ src/main/java/ru/job4j/io/MultiplyTable.java
@@ -3,20 +3,18 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class MultiplyTable {
+
     public static void main(String[] args) {
-        try (FileOutputStream stream = new FileOutputStream("test.txt")) {
-            for (int i = 1; i < 10; i++) {
-                for (int j = 1; j < 10; j++) {
-                    stream.write(Integer.toString(i).getBytes());
-                    stream.write(" * ".getBytes());
-                    stream.write(Integer.toString(j).getBytes());
-                    stream.write(" = ".getBytes());
-                    stream.write(Integer.toString(j * i).getBytes());
-                    stream.write(System.lineSeparator().getBytes());
+            try (FileOutputStream stream = new FileOutputStream("test.txt")) {
+                StringBuilder str = new StringBuilder();
+                for (int i = 1; i < 10; i++) {
+                    for (int j = 1; j < 10; j++) {
+                        str.append(i).append(" * ").append(j).append(" = ").append(i * j).append(" \n");
+                    }
                 }
+                stream.write(str.toString().getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
-}
