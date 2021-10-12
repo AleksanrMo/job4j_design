89a9edf4f6fe44aaef7db3eaf9195b5048320e1d
AleksandrMo <sushanta@mail.ru>
2021-10-12 09:25:19 +0700

    поправил код

--- src/main/java/ru/job4j/question/Analyze.java
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -9,25 +9,24 @@ import java.util.Set;
 public class Analyze {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-
         Info rsl = new Info(0, 0, 0);
         int count = 0;
         Map<Integer, String> mapPrevious = new HashMap<>();
-        for (User user: previous) {
-            mapPrevious.put(user.getId(), user.getName());
+            for (User user: previous) {
+                mapPrevious.put(user.getId(), user.getName());
         }
-       for (User user: current) {
-            if (!mapPrevious.containsKey(user.getId())) {
-                rsl.setAdded(1);
-            } else if (!mapPrevious.get(user.getId()).equals(user.getName())) {
-                rsl.setChanged(1);
-            }
-            if (mapPrevious.containsKey(user.getId())) {
-                count++;
-                if (mapPrevious.size() > count) {
+            for (User user: current) {
+                if (!mapPrevious.containsKey(user.getId())) {
+                    rsl.setAdded(1);
+                } else if (!mapPrevious.get(user.getId()).equals(user.getName())) {
+                    rsl.setChanged(1);
+                    }
+                if (mapPrevious.containsKey(user.getId())) {
+                    count++;
+                    if (mapPrevious.size() > count) {
                     rsl.setDeleted(1);
-                } else {
-                    rsl.setDeleted(0);
+                    } else {
+                        rsl.setDeleted(0);
                 }
             }
         }
