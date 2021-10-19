dcda4ab4543518b717545d166a08ecb4a0f6a92e
AleksandrMo <sushanta@mail.ru>
2021-10-19 20:30:17 +0700

    File

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -11,29 +11,32 @@ import java.util.List;
 public class Analyze {
 
     public void unavailable(String source, String target) {
-        int count = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-             List<String> listFirst = new ArrayList<>();
-             StringBuilder listSec = new StringBuilder();
-             reader.lines().forEach(listFirst::add);
-             for (String s: listFirst) {
-                 String[] str = s.split(" ");
-                    if (count == 0 && (str[0].equals("500") || str[0].equals("400"))) {
-                             listSec.append(str[1]).append(";");
-                             count++;
-                    } else if (count == 1 && (str[0].equals("200") || str[0].equals("300"))) {
-                         listSec.append(str[1]).append(System.lineSeparator());
-                         count = 0;
-                         }
-             }
-             out.write(listSec.toString());
+            int count = 0;
+            while (reader.ready()) {
+                String[] str = reader.readLine().split(" ");
+                if (count == 0 && (str[0].equals("500") || str[0].equals("400"))) {
+                    out.write(str[1] + ";");
+                    count++;
+                } else if (count == 1 && (str[0].equals("200") || str[0].equals("300"))) {
+                    out.write(str[1] + System.lineSeparator());
+                    count = 0;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Analyze analyze = new Analyze();
         analyze.unavailable("./data/sourse.csv", "./data/target.csv");
     }
