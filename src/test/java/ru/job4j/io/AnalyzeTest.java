13caca3cca2a419d8ffdb92d4e834216c9f4f607
AleksandrMo <sushanta@mail.ru>
2021-10-19 09:12:56 +0700

    Тестирование IO

---
+++ src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
+
+public class AnalyzeTest  {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void drop() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+
+        }
+        Analyze analyze = new Analyze();
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01"));
+    }
+}
