a8abb8801269b74d8a2c8c039e2ead0c2cb0b6d3
AleksandrMo <sushanta@mail.ru>
2021-10-20 09:37:50 +0700

    поправил

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -1,35 +1,33 @@
 package ru.job4j.io;
 
-import org.checkerframework.checker.units.qual.A;
-
-import javax.annotation.processing.FilerException;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Analyze {
 
     public void unavailable(String source, String target) {
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             int count = 0;
             while (reader.ready()) {
                 String[] str = reader.readLine().split(" ");
                 if (count == 0 && (str[0].equals("500") || str[0].equals("400"))) {
-                    out.write(str[1] + ";");
+                    builder.append(str[1]).append(";");
                     count++;
                 } else if (count == 1 && (str[0].equals("200") || str[0].equals("300"))) {
-                    out.write(str[1] + System.lineSeparator());
+                    builder.append(str[1]).append(System.lineSeparator());
                     count = 0;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            out.write(builder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
     public static void main(String[] args) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
             out.println("15:01:30;15:02:32");
