fa39bab3262a0f63bf06bfa54279fec75a4a747e
AleksandrMo <sushanta@mail.ru>
2021-10-21 20:05:12 +0700

    доработал код

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,25 +1,26 @@
 package ru.job4j.io.duplicates;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Set<FileProperty> properties = new HashSet<>();
+    Map<FileProperty, List<Path>> map = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
        long size = file.toFile().length();
        String name = file.getFileName().toString();
-        if (!properties.add(new FileProperty(size, name))) {
-            System.out.println("Это дубликат " +  file.toAbsolutePath());
-        }
+       FileProperty property = new FileProperty(size, name);
+        if (map.containsKey(property)) {
+            map.get(property).add(file);
+       }
         return super.visitFile(file, attrs);
     }
 }
