054c3c83d571ecdf8348232177eb0d41f3286cd6
AleksandrMo <sushanta@mail.ru>
2021-10-29 18:00:49 +0700

    LOG

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,11 +8,22 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int numberInt = 789;
+        long numberLong = 78;
+        double numberDouble = 231.6;
+        float numberFloat = 77.7f;
+        boolean flag = true;
+        byte numberByte = 16;
+        short numberShort = 4589;
+        char character = 'a';
 
+        LOG.info("Output of variable : {}", numberByte);
+        LOG.info("Output of variable : {}", numberShort);
+        LOG.info("Output of variable : {}", numberInt);
+        LOG.info("Output of variable : {}", numberLong);
+        LOG.info("Output of variable : {}", numberFloat);
+        LOG.info("Output of variable : {}", numberDouble);
+        LOG.info("Output of variable : {}", flag);
+        LOG.info("Output of variable : {}", character);
     }
 }
