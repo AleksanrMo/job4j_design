fd253a9063d32844347c361f336448e5a2f98c17
AleksandrMo <sushanta@mail.ru>
2021-11-16 13:35:06 +0700

    доработал

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -10,14 +10,15 @@ import java.sql.SQLException;
 public class ConnectionDemo {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
+
         Config config = new Config("./app.properties");
         config.load();
-        System.out.println(config);
+        String driver = config.value("hibernate.connection.driver_class");
         String url = config.value("hibernate.connection.url");
         String login = config.value("hibernate.connection.username");
         String password = config.value("hibernate.connection.password");
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+        Class.forName(driver);
+       try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
            System.out.println(metaData.getUserName());
            System.out.println(metaData.getURL());
