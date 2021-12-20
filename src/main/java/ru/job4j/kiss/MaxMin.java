c60681cbed7e719afd3caf72df0bdef8b4efe4e5
AleksandrMo <sushanta@mail.ru>
2021-12-20 11:14:11 +0700

    доработал

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,19 +1,19 @@
 package ru.job4j.kiss;
 
+import java.io.File;
 import java.util.*;
 
 public class MaxMin {
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T rst = null;
-        if (!value.isEmpty()) {
-            value.sort(comparator);
-            rst = value.get(value.size() - 1);
-        }
-        return rst;
+       return valid(value, comparator.reversed());
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
+       return valid(value, comparator);
+    }
+
+    public <T> T valid(List<T> value, Comparator<T> comparator) {
         T rst = null;
         if (!value.isEmpty()) {
             value.sort(comparator);
