0d94425cf61e40f558a24cea99914ba99bce66ea
AleksandrMo <sushanta@mail.ru>
2021-12-29 01:03:56 +0700

    доработал

--- src/test/java/ru/job4j/solid/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/solid/srp/ReportEngineTest.java
@@ -5,8 +5,9 @@ import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 import ru.job4j.solid.srp.reports.*;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
+import java.util.GregorianCalendar;
 
 public class ReportEngineTest {
 
@@ -29,7 +30,7 @@ public class ReportEngineTest {
     }
 
     @Test
-    public void whenHtmlGenerated() {
+    public void whenHtmlGenerated()  {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
@@ -56,7 +57,7 @@ public class ReportEngineTest {
     }
 
     @Test
-    public void whenHRGenerated() {
+    public void whenHRGenerated()  {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
@@ -76,7 +77,7 @@ public class ReportEngineTest {
     }
 
     @Test
-    public void whenAccountantReport() {
+    public void whenAccountantReport()  {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
@@ -91,6 +92,53 @@ public class ReportEngineTest {
                 .append(worker.getSalary() / 75).append("$").append(";")
                 .append(System.lineSeparator());
         assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenXMLReport()  {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new XMLReport(store);
+        SimpleDateFormat format =
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        StringBuilder string = new StringBuilder()
+
+        .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
+                .append("<employees>\n")
+                .append("    <employee>\n")
+                .append("        <fired>").append(format.format(worker.getHired().getTime())).append("</fired>\n")
+                .append("        <hired>").append(format.format(worker.getHired().getTime())).append("</hired>\n")
+                .append("        <name>Ivan</name>\n")
+                .append("        <salary>100.0</salary>\n")
+                .append("    </employee>\n")
+                .append("</employees>\n");
+
+        assertThat(engine.generate(em -> true), is(string.toString()));
+    }
+
+    @Test
+    public void whenJSONReport()  {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new JSONReport(store);
+        String str = "[{\"name\":\"Ivan\",\"hired\":{\"year\":" + worker.getHired().get(Calendar.YEAR) + ",\"month\":"
+                + worker.getHired().get(Calendar.MONTH) + "," + "\"dayOfMonth\":"
+                + worker.getHired().get(Calendar.DAY_OF_MONTH) + ",\"hourOfDay\":"
+                + worker.getHired().get(Calendar.HOUR_OF_DAY) + ",\"minute\":"
+                + worker.getHired().get(Calendar.MINUTE) + ",\"second\":"
+                + worker.getHired().get(Calendar.SECOND) + "},\"fired\"" + ":{\"year\":"
+                + worker.getHired().get(Calendar.YEAR) + ",\"month\":"
+                + worker.getHired().get(Calendar.MONTH) + ",\"dayOfMonth\":"
+                + worker.getHired().get(Calendar.DAY_OF_MONTH) + ",\"hourOfDay\":"
+                + worker.getHired().get(Calendar.HOUR_OF_DAY) + ",\"minute\":"
+                + worker.getHired().get(Calendar.MINUTE) + ",\"second\":"
+                + worker.getHired().get(Calendar.SECOND) + "},\"salary\":100.0}]";
+
+        assertThat(engine.generate(em -> true), is(str));
     }
 
 }
