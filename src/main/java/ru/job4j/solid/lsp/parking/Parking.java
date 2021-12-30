6676a242e553a067b1b5421b219b24c9eab4ee9d
AleksandrMo <sushanta@mail.ru>
2021-12-30 18:39:15 +0700

    доработал

--- src/main/java/ru/job4j/solid/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/solid/lsp/parking/Parking.java
@@ -2,20 +2,32 @@ package ru.job4j.solid.lsp.parking;
 
 public class Parking implements ParkingLot {
 
-    private int freePlaces;
+    private Cars[] passengerCarPlaces;
+    private Cars[] truckPlaces;
 
-    public Parking(int freePlaces) {
-        this.freePlaces =  freePlaces;
+    public Parking(Cars[] passengerCarPlaces, Cars[] truckPlaces) {
+        this.passengerCarPlaces = passengerCarPlaces;
+        this.truckPlaces = truckPlaces;
     }
-
 
     @Override
     public boolean acceptCar(Cars car) {
         return false;
     }
 
-    public int getFreePlaces() {
-        return freePlaces;
+    public Cars[] getPassengerCarPlaces() {
+        return passengerCarPlaces;
     }
 
+    public void setPassengerCarPlaces(Cars[] passengerCarPlaces) {
+        this.passengerCarPlaces = passengerCarPlaces;
+    }
+
+    public Cars[] getTruckPlaces() {
+        return truckPlaces;
+    }
+
+    public void setTruckPlaces(Cars[] truckPlaces) {
+        this.truckPlaces = truckPlaces;
+    }
 }
--- src/test/java/ru/job4j/solid/lsp/parking/ParkingTest.java
+++ src/test/java/ru/job4j/solid/lsp/parking/ParkingTest.java
@@ -13,18 +13,19 @@ public class ParkingTest {
     public void whenAddPassengerCars() {
 
         Cars passengerCar = new PassengerCar();
-        Parking parking = new Parking(20);
+        Parking parking = new Parking(new Cars[10], new Cars[10]);
         parking.acceptCar(passengerCar);
-        assertEquals(parking.getFreePlaces(), 19);
+        assertEquals(parking.getPassengerCarPlaces()[0], passengerCar);
     }
 
     @Test
     public void whenAddTruck() {
 
         Cars truck = new Truck(2);
-        Parking parking = new Parking(20);
+        Parking parking = new Parking(new Cars[10], new Cars[10]);
         parking.acceptCar(truck);
-        assertEquals(parking.getFreePlaces(), 18);
+        assertEquals(parking.getTruckPlaces()[0], truck);
+        assertEquals(parking.getTruckPlaces()[1], truck);
     }
 
     @Test
@@ -32,26 +33,39 @@ public class ParkingTest {
 
         Cars truck = new Truck(2);
         Cars passengerCar = new PassengerCar();
-        Parking parking = new Parking(20);
+        Parking parking = new Parking(new Cars[10], new Cars[10]);
         parking.acceptCar(truck);
         parking.acceptCar(passengerCar);
-        assertEquals(parking.getFreePlaces(), 17);
+        assertEquals(parking.getTruckPlaces()[0], truck);
+        assertEquals(parking.getTruckPlaces()[1], truck);
+        assertEquals(parking.getPassengerCarPlaces()[0], passengerCar);
     }
 
-
-    @Test(expected = Exception.class)
-    public void whenAddTruckButOnlyOnePlaceOrZero()  {
+    @Test()
+    public void whenAddTruckButOnlyOnePlaceOrZeroInTruckPlaces()  {
 
         Cars truck = new Truck(2);
-        Parking parking = new Parking(1);
+        Parking parking = new Parking(new Cars[10], new Cars[0]);
         parking.acceptCar(truck);
+        assertEquals(parking.getTruckPlaces()[0], truck);
+        assertEquals(parking.getPassengerCarPlaces()[0], truck);
     }
 
     @Test(expected = Exception.class)
-    public void whenAddPassengerCarButPlaceOrZero()  {
-
+    public void whenAddParkingHaveNoPlaceForPassengerCar()  {
         Cars passengerCar = new PassengerCar();
-        Parking parking = new Parking(0);
+        Cars passengerCar2 = new PassengerCar();
+        Parking parking = new Parking(new Cars[0], new Cars[10]);
         parking.acceptCar(passengerCar);
+        parking.acceptCar(passengerCar2);
+    }
+
+    @Test(expected = Exception.class)
+    public void whenAddParkingHaveNoPlaceForTrack()  {
+        Cars passengerCar = new PassengerCar();
+        Cars truck = new Truck(2);
+        Parking parking = new Parking(new Cars[0], new Cars[0]);
+        parking.acceptCar(passengerCar);
+        parking.acceptCar(truck);
     }
 }
