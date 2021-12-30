7af9bc59defb098e90c7343ee04f7261ef4f8e87
AleksandrMo <sushanta@mail.ru>
2021-12-30 16:33:39 +0700

    доработал

--- src/main/java/ru/job4j/solid/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/solid/lsp/parking/Parking.java
@@ -3,7 +3,7 @@ package ru.job4j.solid.lsp.parking;
 public class Parking implements ParkingLot {
 
     @Override
-    public boolean acceptCar() {
+    public boolean acceptCar(Cars car) {
         return false;
     }
 }
